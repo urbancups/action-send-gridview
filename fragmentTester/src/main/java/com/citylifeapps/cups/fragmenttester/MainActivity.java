46e42e1d8e6e57e315270d99d7f6a6b7b3d6d507
Yonatan Moskovich <yonatan@cupsapp.com>
2014-09-11 18:15:35 +0300

    New structure

---
+++ fragmentTester/src/main/java/com/citylifeapps/cups/fragmenttester/MainActivity.java
@@ -0,0 +1,20 @@
+package com.citylifeapps.cups.fragmenttester;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * Created by Yonatan Moskovich on 08/09/2014
+ */
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+
+}
